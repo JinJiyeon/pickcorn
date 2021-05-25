26a5c9f705163cabb796ba1861337bc82fcf6a0e
육승준 <6seungjun@naver.com>
2021-05-25 17:28:26 +0900

    05/25-templates-yuk

--- movies/urls.py
+++ movies/urls.py
@@ -8,13 +8,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     # 단일 영화정보 제공 => detail
     path('<int:movie_pk>/', views.detail, name='detail'),
-    # # 리뷰 생성, 리뷰 정보 반환, 리뷰 수정, 리뷰 삭제 (CRUD)
-    # path('<int:movie_pk>/articles/', views.movie_articles),
-    # path('articles/<int:pk>/', views.article_detail),
-    # # 댓글 생성, 반환
-    # path('comments/', views.comments),
-    # path('comments/<int:pk>/', views.comment_detail),
-    # path('articles/<int:article_pk>/comments/', views.article_comments),
+
     path('<int:movie_pk>/article/', views.article_create, name='article_create'),
     path('article/<int:article_pk>/', views.article_detail, name='article_detail'),
     path('article/<int:article_pk>/update/', views.article_update, name='article_update'),
