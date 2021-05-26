872e246b2a96c0e314581aced126cb9118772150
진지연 <serious_yeon@naver.com>
2021-05-27 02:52:12 +0900

    26-add-detail-jy

--- movies/views.py
+++ movies/views.py
@@ -201,6 +201,7 @@ def detail(request, movie_pk):
 
 
 @require_http_methods(['GET', 'POST'])
+@login_required
 def article_create(request, movie_pk):
     movie = get_object_or_404(Movie, pk=movie_pk)
     if request.method == 'POST':
