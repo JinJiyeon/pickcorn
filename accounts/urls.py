45604994c36156b86db12e9213b4e6874869c0c5
심해영 <shimhy0@naver.com>
2021-05-27 15:01:19 +0900

    0527-last-hy

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,9 +4,11 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
+    # 회원가입 / 로그인 / 로그아웃
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
+
     path('<int:user_pk>/', views.profile, name='profile'),
     path('<int:user_pk>/follow/', views.follow, name='follow'),
     path('<int:user_pk>/followers/', views.followers, name='followers'),
